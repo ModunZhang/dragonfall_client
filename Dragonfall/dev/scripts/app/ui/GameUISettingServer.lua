41c4a48db948efca1dcabaa3c48104889bca4221
Kenny Dai <daidai917@gmail.com>
2015-12-03 21:12:08 +0800

    服务器人口判定逻辑修改

--- Dragonfall/dev/scripts/app/ui/GameUISettingServer.lua
+++ Dragonfall/dev/scripts/app/ui/GameUISettingServer.lua
@@ -111,7 +111,6 @@ function GameUISettingServer:FetchServers()
     NetManager:getServersPromise():done(function(response)
         if response.msg.code == 200 then
             local servers = response.msg.servers
-            dump(servers,"servers")
             self.data = servers
             self:RefreshList()
             self:RefreshServerInfo()
@@ -157,8 +156,10 @@ end
 
 
 function GameUISettingServer:GetStateLableInfoByUserCount(count)
-    if count >= 500 then
+    if count >= 1000 then
         return "HIGH",self.HIGH_COLOR
+    elseif count >= 500 then
+        return "NORMAL",self.LOW_COLOR
     else
         return "LOW",self.LOW_COLOR
     end
@@ -224,7 +225,7 @@ function GameUISettingServer:FillDataItem(content,data)
     content.is_new_server_label:setString(isNew and "[NEW!]" or "")
     content.topAllianceCountry:setTexture(data.serverInfo.alliance and data.serverInfo.alliance ~= json.null and UILib.alliance_language_frame[data.serverInfo.alliance.country] or "icon_unknow_country.png")
     content.top_alliance_label:setString(data.serverInfo.alliance and data.serverInfo.alliance ~= json.null and data.serverInfo.alliance.name or _("无"))
-    local str,color = self:GetStateLableInfoByUserCount(data.serverInfo.totalCount or 0)
+    local str,color = self:GetStateLableInfoByUserCount(data.serverInfo.activeCount or 0)
     content.state_label:setString(str)
     content.state_label:setColor(color)
     if data.id == self.server_code then
