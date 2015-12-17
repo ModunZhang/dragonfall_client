b7f3e6546fada2bf2ca369f5234eb09d46bba3fe
高舟 <gaozhou.f@qq.com>
2015-12-17 20:18:04 +0800

    修改bug

--- Dragonfall/dev/scripts/app/scenes/LoadingScene.lua
+++ Dragonfall/dev/scripts/app/scenes/LoadingScene.lua
@@ -12,13 +12,14 @@ local animation = import("..animation")
 function LoadingScene:onEnter()
 	display.newColorLayer(cc.c4b(255, 255, 255, 255)):addTo(self)
 	self:performWithDelay(function()
-		local manager = ccs.ArmatureDataManager:getInstance()
-	    for k,v in pairs(animation) do
-	        local path = DEBUG_GET_ANIMATION_PATH(string.format("animations/%s.ExportJson", k))
-	        manager:removeArmatureFileInfo(path)
-	    end
-	    cc.Director:getInstance():purgeCachedData()
-	    print("LoadingScene getAppMemoryUsage1", ext.getAppMemoryUsage())
+		if device.platform == 'winrt' then
+			local manager = ccs.ArmatureDataManager:getInstance()
+		    for k,v in pairs(animation) do
+		        local path = DEBUG_GET_ANIMATION_PATH(string.format("animations/%s.ExportJson", k))
+		        manager:removeArmatureFileInfo(path)
+		    end
+		    cc.Director:getInstance():purgeCachedData()
+		end
 	    enter_scene(app:enterScene(self.nextScene, self.args))
 	end, 0.1)
 end
