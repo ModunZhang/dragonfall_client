b2af22480e3e596823bed0b1841f4ac1ff722810
DannyHe <dannyjiajia@gmail.com>
2016-01-15 16:10:34 +0800

    添加java宏CC_USE_TALKING_DATA控制是否启用talkingdata的功能

--- Dragonfall/frameworks/runtime-src/proj.android/src/com/batcatstudio/dragonfall/sdk/MarketSDK.java
+++ Dragonfall/frameworks/runtime-src/proj.android/src/com/batcatstudio/dragonfall/sdk/MarketSDK.java
@@ -1,80 +1,99 @@
 package com.batcatstudio.dragonfall.sdk;
 
 import java.util.HashMap;
-
-import org.cocos2dx.lua.AppActivity;
-
 import android.app.Activity;
-
-import com.tendcloud.tenddata.TDGAAccount;
-import com.tendcloud.tenddata.TDGAItem;
-import com.tendcloud.tenddata.TDGAVirtualCurrency;
-import com.tendcloud.tenddata.TalkingDataGA;
-
+//#ifdef CC_USE_TALKING_DATA
+//@import org.cocos2dx.lua.AppActivity;
+//@import com.tendcloud.tenddata.TDGAAccount;
+//@import com.tendcloud.tenddata.TDGAItem;
+//@import com.tendcloud.tenddata.TDGAVirtualCurrency;
+//@import com.tendcloud.tenddata.TalkingDataGA;
+//#endif
 public class MarketSDK {
-
-	private static String TD_APP_ID = "13842425C091E33B40189A1C9BB6B433"; // test
-	private static String TD_CHANNEL_ID = "All";
-	private static TDGAAccount tdga_account = null;
+	
+//#ifdef CC_USE_TALKING_DATA
+//@	private static String TD_APP_ID = "13842425C091E33B40189A1C9BB6B433"; // test
+//@	private static String TD_CHANNEL_ID = "All";
+//@	private static TDGAAccount tdga_account = null;
+//#endif
 	
 	public static void initSDK() {
-		TalkingDataGA.init(AppActivity.getGameActivity().getApplicationContext(), TD_APP_ID,
-				TD_CHANNEL_ID);
-//#ifndef COCOS_DEBUG
+//#ifdef CC_USE_TALKING_DATA
+//@		TalkingDataGA.init(AppActivity.getGameActivity().getApplicationContext(), TD_APP_ID,
+//@				TD_CHANNEL_ID);
 //@		TalkingDataGA.setVerboseLogDisabled();
 //#endif
 	}
 
 	public static void onPlayerLogin(String playerId, String playerName,
 			String serverName) {
-		
-		TDGAAccount account = TDGAAccount.setAccount(playerId);
-		account.setAccountName(playerName);
-		account.setAccountType(TDGAAccount.AccountType.REGISTERED);
-		account.setGender(TDGAAccount.Gender.UNKNOW);
-		account.setGameServer(serverName);
-		tdga_account = account;
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAAccount account = TDGAAccount.setAccount(playerId);
+//@		account.setAccountName(playerName);
+//@		account.setAccountType(TDGAAccount.AccountType.REGISTERED);
+//@		account.setGender(TDGAAccount.Gender.UNKNOW);
+//@		account.setGameServer(serverName);
+//@		tdga_account = account;
+//#endif
 	}
 
 	public static void onPlayerChargeRequst(String orderID, String productId,
 			double currencyAmount, double virtualCurrencyAmount,
 			String currencyType) {
-		TDGAVirtualCurrency.onChargeRequest(orderID, productId, currencyAmount, currencyType, virtualCurrencyAmount, "Google");
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAVirtualCurrency.onChargeRequest(orderID, productId, currencyAmount, currencyType, virtualCurrencyAmount, "Google");
+//#endif
 	}
 	
 	public static void onPlayerChargeSuccess(String orderID){
-		TDGAVirtualCurrency.onChargeSuccess(orderID);
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAVirtualCurrency.onChargeSuccess(orderID);
+//#endif
 	}
 	
 	public static void onPlayerBuyGameItems(String itemID,int count,double itemPrice){
-		TDGAItem.onPurchase(itemID,count,itemPrice);
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAItem.onPurchase(itemID,count,itemPrice);
+//#endif
 	}
 
 	public static void onPlayerUseGameItems(String itemID,int count) {
-		TDGAItem.onUse(itemID, count);
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAItem.onUse(itemID, count);
+//#endif
 	}
 
 	public static void onPlayerReward(double count,String reason) {
-		TDGAVirtualCurrency.onReward(count,reason);
+//#ifdef CC_USE_TALKING_DATA
+//@		TDGAVirtualCurrency.onReward(count,reason);
+//#endif
 	}
 
 	public static void onPlayerEvent(String event_id,String args) {
 		HashMap<String,String>   hashmap = new HashMap<String,String>();   
 		hashmap.put("desc",args);
-		TalkingDataGA.onEvent(event_id,hashmap);
+//#ifdef CC_USE_TALKING_DATA
+//@		TalkingDataGA.onEvent(event_id,hashmap);
+//#endif
 	}
 
 	public static void onPlayerLevelUp(int level) {
-		if (tdga_account!=null) {
-			tdga_account.setLevel(level);
-		}
+//#ifdef CC_USE_TALKING_DATA
+//@		if (tdga_account!=null) {
+//@			tdga_account.setLevel(level);
+//@		}
+//#endif
 	}
 	//life cycle
 	public static void onResume(Activity activity) {
-		TalkingDataGA.onResume(activity);
+//#ifdef CC_USE_TALKING_DATA
+//@		TalkingDataGA.onResume(activity);
+//#endif
 	}
 	public static void onPause(Activity activity) {
-		TalkingDataGA.onPause(activity);
+//#ifdef CC_USE_TALKING_DATA
+//@		TalkingDataGA.onPause(activity);
+//#endif
 	}
 	
 }
