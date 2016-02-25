f4cb4adf76590cbe29a86e8a58a6a62c30b8aa7b
gaozhou <gaozhou.f@qq.com>
2016-02-25 14:40:44 +0800

    添加低清资源

---
+++ Dragonfall/tools/batcat/buildSDImage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from basic import *
+from batcat import *
+import os
+source = "../../dev/res/images/"
+dest = "../../dev/res/images/sdimages/"
+if __name__=="__main__":
+	D = {}
+	if not os.path.isdir(dest):
+		os.mkdir(dest)
+	for root, dirs, files in os.walk(source):
+		for fileName in files:
+			if "jpg" in fileName and "-sd" not in fileName:
+				command = "%s %s -resize 50%% %s" % (getConvertTool(), source+fileName, dest+fileName.split(".")[0]+"-sd."+fileName.split(".")[1])
+				executeCommand(command, False)
+
+
+
+
+
