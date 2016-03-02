772ab3c058bd0cd4f41feac48d8682827c82c3be
DannyHe <dannyjiajia@gmail.com>
2016-03-02 20:44:26 +0800

    添加java宏定义CC_USE_GOOGLE_PLAY_BILLING_V3

--- Dragonfall/frameworks/runtime-src/proj.android/src/com/batcatstudio/dragonfall/google/billing/StoreKit.java
+++ Dragonfall/frameworks/runtime-src/proj.android/src/com/batcatstudio/dragonfall/google/billing/StoreKit.java
@@ -1,19 +1,20 @@
 package com.batcatstudio.dragonfall.google.billing;
 
-import java.net.URLEncoder;
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@import java.net.URLEncoder;
+//@import java.util.List;
+//@import org.cocos2dx.lib.Cocos2dxHelper;
+//@import org.cocos2dx.lua.AppActivity;
+//@import com.batcatstudio.dragonfall.utils.DebugUtil;
+//@import com.batcatstudio.dragonfall.google.billing.data.SkuDetails;
+//#endif
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
-
-import org.cocos2dx.lib.Cocos2dxHelper;
-import org.cocos2dx.lua.AppActivity;
-
 import com.batcatstudio.dragonfall.google.billing.data.Inventory;
-import com.batcatstudio.dragonfall.google.billing.data.SkuDetails;
 import com.batcatstudio.dragonfall.utils.CommonUtils;
-import com.batcatstudio.dragonfall.utils.DebugUtil;
-
 import android.content.Intent;
+
 @SuppressWarnings("deprecation")
 public class StoreKit {
 
@@ -39,102 +40,107 @@ public class StoreKit {
 	private static native void onPurchaseFailed();
 	
 	public static void updateTransactionStates(final ArrayList<String> skuArray) {
-		if (DEBUG) {
-			DebugUtil.LogInfo(TAG, "updateTransactionStates: " + skuArray);
-		}
-		if(mHelper!=null && mHelper.iapSupported() &&  skuArray.size() > 0){
-			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					mHelper.queryInventoryAsync(true, skuArray, mGotInventoryListener);
-				}
-			});
-		}
-		
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (DEBUG) {
+//@			DebugUtil.LogInfo(TAG, "updateTransactionStates: " + skuArray);
+//@		}
+//@		if(mHelper!=null && mHelper.iapSupported() &&  skuArray.size() > 0){
+//@			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
+//@				@Override
+//@				public void run() {
+//@					mHelper.queryInventoryAsync(true, skuArray, mGotInventoryListener);
+//@				}
+//@			});
+//@		}
+//#endif
 	}
 	
 	public static void requestProductData(Set<String> itemSkuSet) {
-		if (DEBUG) {
-			DebugUtil.LogInfo(TAG, "requestProductData: " + itemSkuSet);
-		}
-		if (currentInv.getAllSkuSet().size() > 0) {
-			String[] itemIds = new String[itemSkuSet.size()];
-			String[] itemPrices = new String[itemSkuSet.size()];
-			int i = 0;
-			for (String sku : itemSkuSet) {
-				if (currentInv.hasDetails(sku)) {
-					SkuDetails details = currentInv.getSkuDetails(sku);
-					itemIds[i] = sku;
-					itemPrices[i++] = details.getPrice();
-					if (DEBUG)
-						DebugUtil.LogInfo(TAG, String.format("itemId: %s, itemPrice: %s", sku, details.getPrice()));
-				} else {
-					DebugUtil.LogErr(TAG, "requestProductData not exist, sku: " + sku);
-				}
-			}
-			productDataReceived(itemIds, itemPrices);
-		}
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (DEBUG) {
+//@			DebugUtil.LogInfo(TAG, "requestProductData: " + itemSkuSet);
+//@		}
+//@		if (currentInv.getAllSkuSet().size() > 0) {
+//@			String[] itemIds = new String[itemSkuSet.size()];
+//@			String[] itemPrices = new String[itemSkuSet.size()];
+//@			int i = 0;
+//@			for (String sku : itemSkuSet) {
+//@				if (currentInv.hasDetails(sku)) {
+//@					SkuDetails details = currentInv.getSkuDetails(sku);
+//@					itemIds[i] = sku;
+//@					itemPrices[i++] = details.getPrice();
+//@					if (DEBUG)
+//@						DebugUtil.LogInfo(TAG, String.format("itemId: %s, itemPrice: %s", sku, details.getPrice()));
+//@				} else {
+//@					DebugUtil.LogErr(TAG, "requestProductData not exist, sku: " + sku);
+//@				}
+//@			}
+//@			productDataReceived(itemIds, itemPrices);
+//@		}
+//#endif
 	}
 
 	public static void buy(final String sku) {
-		if (mHelper.iapSupported()) {
-			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						mHelper.launchPurchaseFlow(AppActivity.getGameActivity(), sku, ++latestRequestCode,
-								mPurchaseFinishedListener);
-					} catch (Exception e) {
-						DebugUtil.LogException(TAG, e);
-						AppActivity.getGameActivity().runOnGLThread(new Runnable() {
-							@Override
-							public void run() {
-								onPurchaseFailed();
-							}
-						});
-					}
-				}
-			});
-		}
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (mHelper.iapSupported()) {
+//@			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
+//@				@Override
+//@				public void run() {
+//@					try {
+//@						mHelper.launchPurchaseFlow(AppActivity.getGameActivity(), sku, ++latestRequestCode,
+//@								mPurchaseFinishedListener);
+//@					} catch (Exception e) {
+//@						DebugUtil.LogException(TAG, e);
+//@						AppActivity.getGameActivity().runOnGLThread(new Runnable() {
+//@							@Override
+//@							public void run() {
+//@								onPurchaseFailed();
+//@							}
+//@						});
+//@					}
+//@				}
+//@			});
+//@		}
+//#endif
 	}
-
-	private static IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-		public void onIabPurchaseFinished(IabResult result, final Purchase purchase) {
-			if (DEBUG)
-				DebugUtil.LogDebug(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
-			if (result.isFailure()) {
-				DebugUtil.LogErr(TAG, String.format("Purchase failed, error info: %s", result.getMessage()));
-				AppActivity.getGameActivity().runOnGLThread(new Runnable() {
-					@Override
-					public void run() {
-						onPurchaseFailed();
-					}
-				});
-				return;
-			}
-			if (!verifyDeveloperPayload(purchase)) {
-				DebugUtil.LogErr(TAG, "Error purchasing: verifyDeveloperPayload failed.");
-				return;
-			}
-			currentInv.addPurchase(purchase);
-
-			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					AppActivity.getGameActivity()
-							.showDialog(AppActivity.AppActivityDialog.DIALOG_PAYMENT_PURCHASED.ordinal());
-				}
-			});
-			AppActivity.getGameActivity().runOnGLThread(new Runnable() {
-				@Override
-				public void run() {
-					verifyGPV3Purchase(purchase.getOrderId(), purchase.getOriginalJson(),
-							purchase.getSignature());
-				}
-			});
-		}
-	};
-
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@	private static IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+//@		public void onIabPurchaseFinished(IabResult result, final Purchase purchase) {
+//@			if (DEBUG)
+//@				DebugUtil.LogDebug(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+//@			if (result.isFailure()) {
+//@				DebugUtil.LogErr(TAG, String.format("Purchase failed, error info: %s", result.getMessage()));
+//@				AppActivity.getGameActivity().runOnGLThread(new Runnable() {
+//@					@Override
+//@					public void run() {
+//@						onPurchaseFailed();
+//@					}
+//@				});
+//@				return;
+//@			}
+//@			if (!verifyDeveloperPayload(purchase)) {
+//@				DebugUtil.LogErr(TAG, "Error purchasing: verifyDeveloperPayload failed.");
+//@				return;
+//@			}
+//@			currentInv.addPurchase(purchase);
+//@
+//@			AppActivity.getGameActivity().runOnUiThread(new Runnable() {
+//@				@Override
+//@				public void run() {
+//@					AppActivity.getGameActivity()
+//@							.showDialog(AppActivity.AppActivityDialog.DIALOG_PAYMENT_PURCHASED.ordinal());
+//@				}
+//@			});
+//@			AppActivity.getGameActivity().runOnGLThread(new Runnable() {
+//@				@Override
+//@				public void run() {
+//@					verifyGPV3Purchase(purchase.getOrderId(), purchase.getOriginalJson(),
+//@							purchase.getSignature());
+//@				}
+//@			});
+//@		}
+//@	};
+//#endif
 	/** Verifies the developer payload of a purchase. */
 	private static boolean verifyDeveloperPayload(Purchase p) {
 		return true;
@@ -143,46 +149,53 @@ public class StoreKit {
 	private static long currentDelay = 0l;
 
 	public static void consumePurchase(final String orderId) {
-		if (orderId.length() != 0 && currentInv.hasPurchase(orderId)) {
-			if (DEBUG) {
-				DebugUtil.LogDebug(TAG, "consumePurchase: " + currentInv.getPurchase(orderId));
-			}
-			try {
-				AppActivity.gameHandler.postDelayed(new Runnable() {
-					@Override
-					public void run() {
-						mHelper.consumeAsync(currentInv.getPurchase(orderId), mConsumeFinishedListener);
-					}
-				}, currentDelay);
-				currentDelay += 100;
-			} catch (Exception e) {
-				DebugUtil.LogException(TAG, e);
-			}
-		}
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (orderId.length() != 0 && currentInv.hasPurchase(orderId)) {
+//@			if (DEBUG) {
+//@				DebugUtil.LogDebug(TAG, "consumePurchase: " + currentInv.getPurchase(orderId));
+//@			}
+//@			try {
+//@				AppActivity.gameHandler.postDelayed(new Runnable() {
+//@					@Override
+//@					public void run() {
+//@						mHelper.consumeAsync(currentInv.getPurchase(orderId), mConsumeFinishedListener);
+//@					}
+//@				}, currentDelay);
+//@				currentDelay += 100;
+//@			} catch (Exception e) {
+//@				DebugUtil.LogException(TAG, e);
+//@			}
+//@		}
+//#endif
 	}
-
-	private static IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
-		public void onConsumeFinished(Purchase purchase, IabResult result) {
-			if (DEBUG)
-				DebugUtil.LogDebug(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
-			if (result.isSuccess()) {
-				if (DEBUG) {
-					DebugUtil.LogDebug(TAG, "Consumption successful. Provisioning.");
-				}
-				currentInv.erasePurchase(purchase.getOrderId());
-
-			} else {
-				if (DEBUG) {
-					DebugUtil.LogErr(TAG, "Consumption faild. Error info: " + result.getMessage());
-				}
-			}
-		}
-	};
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@	private static IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+//@		public void onConsumeFinished(Purchase purchase, IabResult result) {
+//@			if (DEBUG)
+//@				DebugUtil.LogDebug(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+//@			if (result.isSuccess()) {
+//@				if (DEBUG) {
+//@					DebugUtil.LogDebug(TAG, "Consumption successful. Provisioning.");
+//@				}
+//@				currentInv.erasePurchase(purchase.getOrderId());
+//@
+//@			} else {
+//@				if (DEBUG) {
+//@					DebugUtil.LogErr(TAG, "Consumption faild. Error info: " + result.getMessage());
+//@				}
+//@			}
+//@		}
+//@	};
+	//#endif
 
 	// can buy ?
 	public static boolean isGMSSupport() {
 		//google play service and  billing v3  
-		return mHelper.iapSupported();
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		return mHelper.iapSupported();
+//#else
+		return false;
+//#endif
 	}
 
 	public static void getGMSSupport() {
@@ -191,82 +204,88 @@ public class StoreKit {
 
 	public static void init() {
 		initJNI();
-		mHelper = new IabHelper(AppActivity.getGameActivity());
-		currentInv = new Inventory();
-
-		mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-			public void onIabSetupFinished(IabResult result) {
-				if (!result.isSuccess()) {
-					if (DEBUG) {
-						DebugUtil.LogErr(TAG, "Problem setting up in-app billing: " + result);
-					}
-					return;
-				}
-			}
-		});
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		mHelper = new IabHelper(AppActivity.getGameActivity());
+//@		currentInv = new Inventory();
+//@
+//@		mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+//@			public void onIabSetupFinished(IabResult result) {
+//@				if (!result.isSuccess()) {
+//@					if (DEBUG) {
+//@						DebugUtil.LogErr(TAG, "Problem setting up in-app billing: " + result);
+//@					}
+//@					return;
+//@				}
+//@			}
+//@		});
+//#endif
 	}
-
-	private static IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-		public void onQueryInventoryFinished(IabResult result, final Inventory inventory) {
-			if (result.isFailure()) {
-				if (DEBUG)
-					DebugUtil.LogErr(TAG, "Failed to query inventory: " + result);
-				return;
-			}
-			if (DEBUG)
-				DebugUtil.LogDebug(TAG, "Query inventory was successful.");
-			currentInv = inventory;
-
-			final List<Purchase> unverifiedPurchases = inventory.getAllPurchases();
-			if (DEBUG)
-				DebugUtil.LogDebug(TAG, "before unverifiedPurchases.size() > 0");
-			if (unverifiedPurchases.size() > 0) {
-				if (DEBUG) {
-					DebugUtil.LogInfo(TAG, "Got unverified purchases: " + unverifiedPurchases);
-				}
-				AppActivity.gameHandler.postDelayed(new Runnable() {
-					@Override
-					public void run() {
-						for (final Purchase purchase : unverifiedPurchases) {
-							if (verifyDeveloperPayload(purchase)) {
-								Cocos2dxHelper.runOnGLThread(new Runnable() {
-									@Override
-									public void run() {
-										if (DEBUG)
-											DebugUtil.LogDebug(TAG, "before verifyGPV3Purchase");
-										if (DEBUG)
-											DebugUtil.LogDebug(TAG, purchase.getOrderId());
-										if (DEBUG)
-											DebugUtil.LogDebug(TAG, purchase.getOriginalJson());
-										if (DEBUG)
-											DebugUtil.LogDebug(TAG, URLEncoder.encode(purchase.getSignature()));
-										verifyGPV3Purchase(purchase.getOrderId(), purchase.getOriginalJson(),
-												purchase.getSignature());
-									}
-								});
-							}
-						}
-					}
-				}, VERIFY_PURCHASE_DELAY);
-			}
-		}
-	};
-
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@	private static IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+//@		public void onQueryInventoryFinished(IabResult result, final Inventory inventory) {
+//@			if (result.isFailure()) {
+//@				if (DEBUG)
+//@					DebugUtil.LogErr(TAG, "Failed to query inventory: " + result);
+//@				return;
+//@			}
+//@			if (DEBUG)
+//@				DebugUtil.LogDebug(TAG, "Query inventory was successful.");
+//@			currentInv = inventory;
+//@
+//@			final List<Purchase> unverifiedPurchases = inventory.getAllPurchases();
+//@			if (DEBUG)
+//@				DebugUtil.LogDebug(TAG, "before unverifiedPurchases.size() > 0");
+//@			if (unverifiedPurchases.size() > 0) {
+//@				if (DEBUG) {
+//@					DebugUtil.LogInfo(TAG, "Got unverified purchases: " + unverifiedPurchases);
+//@				}
+//@				AppActivity.gameHandler.postDelayed(new Runnable() {
+//@					@Override
+//@					public void run() {
+//@						for (final Purchase purchase : unverifiedPurchases) {
+//@							if (verifyDeveloperPayload(purchase)) {
+//@								Cocos2dxHelper.runOnGLThread(new Runnable() {
+//@									@Override
+//@									public void run() {
+//@										if (DEBUG)
+//@											DebugUtil.LogDebug(TAG, "before verifyGPV3Purchase");
+//@										if (DEBUG)
+//@											DebugUtil.LogDebug(TAG, purchase.getOrderId());
+//@										if (DEBUG)
+//@											DebugUtil.LogDebug(TAG, purchase.getOriginalJson());
+//@										if (DEBUG)
+//@											DebugUtil.LogDebug(TAG, URLEncoder.encode(purchase.getSignature()));
+//@										verifyGPV3Purchase(purchase.getOrderId(), purchase.getOriginalJson(),
+//@												purchase.getSignature());
+//@									}
+//@								});
+//@							}
+//@						}
+//@					}
+//@				}, VERIFY_PURCHASE_DELAY);
+//@			}
+//@		}
+//@	};
+//#endif
 	public static boolean handleActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode > BASE_REQUEST_CODE) {
-			mHelper.handleActivityResult(requestCode, resultCode, data);
-			return true;
-		}
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (requestCode > BASE_REQUEST_CODE) {
+//@			mHelper.handleActivityResult(requestCode, resultCode, data);
+//@			return true;
+//@		}
+//#endif
 		return false;
 	}
 
 	public static void purge() {
-		if (mHelper != null)
-			mHelper.dispose();
-		mHelper = null;
-		currentInv = null;
-		mPurchaseFinishedListener = null;
-		mGotInventoryListener = null;
-		mConsumeFinishedListener = null;
+//#ifdef CC_USE_GOOGLE_PLAY_BILLING_V3
+//@		if (mHelper != null)
+//@			mHelper.dispose();
+//@		mHelper = null;
+//@		currentInv = null;
+//@		mPurchaseFinishedListener = null;
+//@		mGotInventoryListener = null;
+//@		mConsumeFinishedListener = null;
+//#endif
 	}
 }
