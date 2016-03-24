8959d2a8ca392f7bdadff25068856c68438638e7
DannyHe <dannyjiajia@gmail.com>
2016-03-24 15:10:28 +0800

    构建测试apk

--- Dragonfall/frameworks/runtime-src/proj.android-studio/app/src/com/batcatstudio/dragonfall/data/DataHelper.java
+++ Dragonfall/frameworks/runtime-src/proj.android-studio/app/src/com/batcatstudio/dragonfall/data/DataHelper.java
@@ -47,7 +47,7 @@ public class DataHelper {
 	
 	private static String PREFERENCES_NAME = "com.batcatstudio.game.preferences." + BuildConfig.GAME_ID;
 	
-	public static final long ZIP_RESOURCE_SIZE = 74310290;
+	public static final long ZIP_RESOURCE_SIZE = 67226159;
 
 	private static int appVersionCode = -1;
 
