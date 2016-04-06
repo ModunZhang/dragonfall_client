5bd3fdbf5da83b480bd05d2cedfa82e04bcf6f3b
高舟 <gaozhou.f@qq.com>
2016-04-06 17:53:16 +0800

    添加游乐场动画

--- Dragonfall/dev/scripts/app/layers/CityLayer.lua
+++ Dragonfall/dev/scripts/app/layers/CityLayer.lua
@@ -522,7 +522,7 @@ function CityLayer:InitWithCity(city)
     -- pve 入口
     self.watchTower = WatchTowerSprite.new(self, -7, 15):addTo(city_node)
     self.pve_airship = AirshipSprite.new(self, -7, 2):addTo(city_node)
-    self.fair_ground = FairGroundSprite.new(self, -5, 8):addTo(city_node)
+    self.fair_ground = FairGroundSprite.new(self, -5, 9):addTo(city_node)
 
 
     -- 更新其他需要动态生成的建筑
