80432de661dffe22b931574b42eba021211859d0
gaozhou <gaozhou.f@qq.com>
2016-04-08 09:53:37 +0800

    修改winphone bug

--- Dragonfall/dev/scripts/app/scenes/LoadingScene.lua
+++ Dragonfall/dev/scripts/app/scenes/LoadingScene.lua
@@ -6,21 +6,23 @@ function LoadingScene:ctor(...)
 	local args = {...}
 	self.nextScene = table.remove(args, 1)
 	self.args = args
+	self.currentScene = display.getRunningScene().__cname
 end
 
 -- local animation = import("..animation")
 function LoadingScene:onEnter()
 	display.newColorLayer(cc.c4b(255, 255, 255, 255)):addTo(self)
 	self:performWithDelay(function()
-		-- collectgarbage("collect")
-		-- if device.platform == 'winrt' then
+		if device.platform == 'winrt' and self.currentScene ~= self.nextScene then
+			print("=======================================>>")
 		-- -- 	local manager = ccs.ArmatureDataManager:getInstance()
 		-- --     for k,v in pairs(animation) do
 		-- --         local path = DEBUG_GET_ANIMATION_PATH(string.format("animations/%s.ExportJson", k))
 		-- --         manager:removeArmatureFileInfo(path)
 		-- --     end
-		--     cc.Director:getInstance():purgeCachedData()
-		-- end
+		    cc.Director:getInstance():purgeCachedData()
+		end
+		collectgarbage("collect")
 	    enter_scene(app:enterScene(self.nextScene, self.args))
 	end, 0.01)
 end
