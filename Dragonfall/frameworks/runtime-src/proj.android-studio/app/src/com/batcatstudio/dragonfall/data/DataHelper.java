61ea77c033c634f3468435d65f7ec3e73d96837c
DannyHe <dannyjiajia@gmail.com>
2016-04-22 19:34:34 +0800

    android 1.1.3 119打包资源

--- Dragonfall/frameworks/runtime-src/proj.android-studio/app/src/com/batcatstudio/dragonfall/data/DataHelper.java
+++ Dragonfall/frameworks/runtime-src/proj.android-studio/app/src/com/batcatstudio/dragonfall/data/DataHelper.java
@@ -47,7 +47,7 @@ public class DataHelper {
 	
 	private static String PREFERENCES_NAME = "com.batcatstudio.game.preferences." + BuildConfig.GAME_ID;
 	
-	public static final long ZIP_RESOURCE_SIZE = 74532052;
+	public static final long ZIP_RESOURCE_SIZE = 74719496;
 
 	private static int appVersionCode = -1;
 
