0bcc8779a5f904ebe791e3f158a92c9d583932e1
高舟 <gaozhou.f@qq.com>
2016-05-10 14:48:22 +0800

    修改大图导小图脚本

--- Dragonfall/tools/batcat/buildSDImage.py
+++ Dragonfall/tools/batcat/buildSDImage.py
@@ -3,9 +3,21 @@
 
 from basic import *
 from batcat import *
+import filecmp 
 import os
+# filecmp.cmp(r'e:\1.txt',r'e:\2.txt')
+origin= "../../dev/res/images/"
 source = "../../dev/res/images/hdimages/"
 dest = "../../dev/res/images/sdimages/"
+
+
+def emitSdImage(inFile):
+	fileName = os.path.basename(inFile)
+	outFile = dest+fileName.split(".")[0]+"-sd."+fileName.split(".")[1]
+	if fileNewer(inFile, outFile):
+		command = "%s %s -resize 50%% %s" % (getConvertTool(), inFile, dest+fileName.split(".")[0]+"-sd."+fileName.split(".")[1])
+		executeCommand(command, False)
+
 if __name__=="__main__":
 	D = {}
 	if not os.path.isdir(dest):
@@ -14,11 +26,18 @@ if __name__=="__main__":
 		for fileName in files:
 			fileExt = fileName.split(".")[-1]
 			if "-sd" not in fileName and fileExt != 'DS_Store':
-				file1 = source+fileName
-				file2 = dest+fileName.split(".")[0]+"-sd."+fileName.split(".")[1]
-				if fileNewer(file1, file2):
-					command = "%s %s -resize 50%% %s" % (getConvertTool(), source+fileName, dest+fileName.split(".")[0]+"-sd."+fileName.split(".")[1])
-					executeCommand(command, False)
+				if os.path.exists(origin+fileName):
+					if not filecmp.cmp(origin+fileName, source+fileName):
+						print("not equal", fileName)
+						os.remove(source+fileName)
+						shutil.copy(origin+fileName,source+fileName) 
+						print("copy", fileName)
+					else:
+						emitSdImage(source+fileName)
+				else:
+					print("not found", fileName)
+					os.remove(source+fileName)
+					print("remove", fileName)
 
 
 
