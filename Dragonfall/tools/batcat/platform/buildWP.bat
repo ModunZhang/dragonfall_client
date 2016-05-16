c9a5bb6a18b1f687bdf6a1475d9539206a9f5d45
DannyHe <dannyjiajia@gmail.com>
2016-05-16 20:08:35 +0800

    优化脚本

--- Dragonfall/tools/batcat/platform/buildWP.bat
+++ Dragonfall/tools/batcat/platform/buildWP.bat
@@ -1,6 +1,6 @@
 :: build the Windows Phone Project for visual studio 2013
 :: by dannyhe
-
+:: useage: buildWP.bat [OutDir]
 @echo off
 echo./*
 echo. * Check VC++ environment...
@@ -8,6 +8,9 @@ echo. */
 echo.
 
 set FOUND_VC=0
+set FOUND_OUTDIR=0
+
+if not "%~1"=="" set FOUND_OUTDIR=1
 
 if defined VS120COMNTOOLS (
     set VSTOOLS="%VS120COMNTOOLS%"
@@ -28,7 +31,11 @@ echo. */
 echo.
 call %VSVARS%
 if %FOUND_VC%==1 (
-msbuild  ..\..\..\frameworks\runtime-src\proj.win8.1-universal\App.WindowsPhone\Dragonfall.WindowsPhone.vcxproj /p:Configuration="Release"  /p:Platform="ARM" /t:Clean;Rebuild
+	if %FOUND_OUTDIR%==0 (
+		msbuild  ..\..\..\frameworks\runtime-src\proj.win8.1-universal\App.WindowsPhone\Dragonfall.WindowsPhone.vcxproj /p:Configuration="Release"  /p:Platform="ARM" /t:Clean;Rebuild
+	) else (
+		msbuild  ..\..\..\frameworks\runtime-src\proj.win8.1-universal\App.WindowsPhone\Dragonfall.WindowsPhone.vcxproj /p:Configuration="Release"  /p:Platform="ARM" /t:Clean;Rebuild /p:OutDir=%~f1
+	)
 ) else (
     echo Script error.
     goto ERROR
