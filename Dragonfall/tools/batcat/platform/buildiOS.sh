183537feaf978480a637638200c57a48e63502dd
DannyHe <dannyjiajia@gmail.com>
2016-08-15 19:09:44 +0800

    添加说明

--- Dragonfall/tools/batcat/platform/buildiOS.sh
+++ Dragonfall/tools/batcat/platform/buildiOS.sh
@@ -1,9 +1,17 @@
 #!/bin/bash --login
 #---------------------------------------------------
-# build the iOS project with xcode command line tools
 # Date: 2016/08/15
 # Version: 1.0.0 beta
 # by dannyhe
+#---------------------------------------------------
+## 使用
+# mac下通过`xcodebuild`自动打包并生成ipa文件的脚本,并提供将打包文件导出为ipa的功能.无需关心Xcode中的证书配置,脚本自动修改.但是必须提前安装好证书的配置.
+# 1. buildiOS.sh xxx.xcarchive ./output Inhouse # 表示将xxx.xcarchive在Inhouse模式下生成ipa文件到output目录.
+# 2. 其他形式的调用(3个参数的情况除外)会运行打包项目操作并生成相应的ipa文件.
+## 注意:
+# 1.脚本导出app store包的时候强制包含符号文件 .
+# 2.TeamID定值为9RNQD8JEQ2.
+# 3.bitcode在任何模式下关闭.
 #---------------------------------------------------
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # current dir path
@@ -15,9 +23,8 @@ SchemeName="DragonWar"
 #---------------------------------------------------
 # Project 
 PrjDir="${DIR}/../../../frameworks/runtime-src/proj.ios_mac" # project path
-PrjName="Dragonfall" # xcode project filename: Dragonfall.xcodeproj
 InfoPlistPath="${PrjDir}/ios/Info.plist"
-ExportOptionsPlistPath="${DIR}/iOSExportOptions.plist" # for script
+ExportOptionsPlistPath="${DIR}/iOSExportOptions.plist" # for xcode
 #---------------------------------------------------
 # Xcode
 TeamID="9RNQD8JEQ2" # The Developer Portal team to use for this export.
