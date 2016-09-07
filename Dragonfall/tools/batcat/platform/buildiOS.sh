0e869e68bb7ba66020d120d223e69a8ce9fabe57
DannyHe <dannyjiajia@gmail.com>
2016-09-07 10:11:07 +0800

    ios打包脚本修改

--- Dragonfall/tools/batcat/platform/buildiOS.sh
+++ Dragonfall/tools/batcat/platform/buildiOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 #---------------------------------------------------
 # Date: 2016/08/15
-# Version: 1.0.0 beta
+# Version: 1.0.0
 # by dannyhe
 #---------------------------------------------------
 ## 使用
@@ -10,14 +10,13 @@
 # 2. 其他形式的调用(3个参数的情况除外)会运行打包项目操作并生成相应的ipa文件.
 ## 注意:
 # 1.脚本导出app store包的时候强制包含符号文件 .
-# 2.TeamID定值为9RNQD8JEQ2.
-# 3.bitcode在任何模式下关闭.
+# 2.bitcode在任何模式下关闭.
 #---------------------------------------------------
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # current dir path
 #---------------------------------------------------
 # build argments
-ProvisionType="Distribution" # Development/Inhouse/Distribution. defalts Distribution
+# ProvisionType="Distribution" # Development/Inhouse/Distribution. defalts Distribution
 TargetName="DragonfallWar"
 SchemeName="DragonWar"
 #---------------------------------------------------
@@ -33,12 +32,16 @@ DistributionCodeIdentity="iPhone Distribution: Pin Wen Huang (9RNQD8JEQ2)" # Inh
 # Provision
 DistributionProvision="65ab5db9-8a57-4a7d-863e-260db8d96c56" # Distribution profile identifier
 
-BuildVer=`/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" $InfoPlistPath`
-OutputName=`date "+%Y-%m-%d_%H_%M_%S"`_${TargetName}_${BuildVer}
-ArchiveFileFullPath=${PrjDir}/Archive/${OutputName}.xcarchive
-IpaFileDirectory=${PrjDir}/Archive/Outputs/${OutputName}
+#---------------------------------------------------
+# Main function
+#---------------------------------------------------
 
-function getProvisionMethod()
+BuildVer=`/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" $InfoPlistPath`
+OutputName=${TargetName}_${BuildVer}
+OutputDirPath=${PrjDir}/Output/${OutputName}
+
+# convert ProvisionType to Xcode method
+function getProvisionMethod2PlistConfig()
 {
 	case $1 in 
 		Distribution)
@@ -54,7 +57,7 @@ function getProvisionMethod()
 			echo "app-store"
 	esac
 }
-
+# Xcode config
 function setBuildMethod()
 {
 	/usr/libexec/PlistBuddy -c "set method $1" $ExportOptionsPlistPath
@@ -67,54 +70,78 @@ function getBuildMethod()
 
 function setBuildTeamID()
 {
-	/usr/libexec/PlistBuddy -c "set method ${TeamID}" $ExportOptionsPlistPath
+	/usr/libexec/PlistBuddy -c "set teamID ${TeamID}" $ExportOptionsPlistPath
+}
+
+# project config
+
+function setAppHoc()
+{
+	/usr/libexec/PlistBuddy -c "set AppHoc $1" $InfoPlistPath
+}
+
+function printAppHoc()
+{
+	echo "---------------------------------------------------------"
+	echo "appHoc:"
+	echo `/usr/libexec/PlistBuddy -c 'print AppHoc' $InfoPlistPath`
+	echo "---------------------------------------------------------"
+}
+
+
+function archiveAndExportProject()
+{
+	iApphoc=$1
+	iArchiveDirName="Release_Server"
+	iTimestamp=`date "+%Y-%m-%d_%H_%M_%S"`
+	if [ "$iApphoc" = "true" ];then
+		iArchiveDirName="Debug_Server"
+	fi
+	iFinalOutputArchiveName="${OutputName}_${iArchiveDirName}_${iTimestamp}.xcarchive"
+	iArchiveFileFullPath="${OutputDirPath}/Archive/${iFinalOutputArchiveName}"
+	setAppHoc ${iApphoc}
+	archiveProject ${iArchiveFileFullPath}
+	iFinalOutputIpaDirPath="${OutputDirPath}/Archive/IPAs/${iArchiveDirName}_${iTimestamp}"
+	exportArchive2IpaWithConfig ${iArchiveFileFullPath} "${iFinalOutputIpaDirPath}_Inhouse" "Inhouse"
+	if [ "$iApphoc" = "false" ];then
+		exportArchive2IpaWithConfig ${iArchiveFileFullPath} "${iFinalOutputIpaDirPath}_Distribution" "Distribution"
+	fi
 }
 
 function archiveProject()
 {
+	iArchiveFileFullPath=$1
+	printAppHoc
 	cd $PrjDir
 	xcodebuild clean
-	xcodebuild -sdk iphoneos -configuration Release -scheme ${SchemeName} -target "${TargetName}" -archivePath ${ArchiveFileFullPath} CODE_SIGN_IDENTITY="${DistributionCodeIdentity}" PROVISIONING_PROFILE="${DistributionProvision}" archive
+	xcodebuild -sdk iphoneos -configuration Release -scheme ${SchemeName} -target "${TargetName}" -archivePath ${iArchiveFileFullPath} CODE_SIGN_IDENTITY="${DistributionCodeIdentity}" PROVISIONING_PROFILE="${DistributionProvision}" archive
 	cd $DIR
 }
 
-function exportArchive()
+function normalExportProject()
 {
-	BakMethod=$(getBuildMethod)
-	setBuildMethod $(getProvisionMethod $ProvisionType)
-	echo "---------------------------------------------------"
-	/usr/libexec/PlistBuddy -c 'print' ${ExportOptionsPlistPath}
-	echo "---------------------------------------------------"
-	xcodebuild -exportArchive -exportOptionsPlist ${ExportOptionsPlistPath} -archivePath ${ArchiveFileFullPath} -exportPath ${IpaFileDirectory}
-	setBuildMethod $BakMethod
-	/usr/libexec/PlistBuddy -c 'print' ${ExportOptionsPlistPath}
+	echo "-------- export debug project --------"
+	read -p "Press [Enter] key to start..."
+	archiveAndExportProject true
+
+	echo "-------- export release project --------"
+	read -p "Press [Enter] key to start..."
+	archiveAndExportProject false
 }
 
-function normalArchiveAndExport()
+function exportArchive2IpaWithConfig()
 {
-	echo "--Begin--"
-	echo "Archive"
-	echo "---------------------------------------------------"
-	archiveProject
-	echo "PackageApplication"
-	echo "---------------------------------------------------"
-	exportArchive
-	echo "---------------------------------------------------"
-	echo "--End--"
-}
-
-function exportArchiveWithConfig()
-{
-	echo "--Begin--"
+	echo "-------- export ipa --------"
 	iArchiveFileFullPath=$1
 	iIpaFileDirectory=$2
 	iProvisionType=$3
 	BakMethod=$(getBuildMethod)
-	setBuildMethod $(getProvisionMethod $iProvisionType)
+	setBuildMethod $(getProvisionMethod2PlistConfig $iProvisionType)
+	setBuildTeamID 
 	echo "---------------------------------------------------"
 	/usr/libexec/PlistBuddy -c 'print' ${ExportOptionsPlistPath}
 	echo "---------------------------------------------------"
-	xcodebuild -exportArchive -exportOptionsPlist ${ExportOptionsPlistPath} -archivePath ${iArchiveFileFullPath} -exportPath ${iIpaFileDirectory}
+	xcodebuild -exportArchive -exportOptionsPlist ${ExportOptionsPlistPath} -archivePath ${iArchiveFileFullPath} -exportPath "${iIpaFileDirectory}"
 	setBuildMethod $BakMethod
 	/usr/libexec/PlistBuddy -c 'print' ${ExportOptionsPlistPath}
 	echo "--End--"
@@ -122,7 +149,7 @@ function exportArchiveWithConfig()
 
 #---------------------------------------------------
 if [ $# -eq 3 ];then
-	exportArchiveWithConfig $@
+	exportArchive2IpaWithConfig $@
 else
-	normalArchiveAndExport
+	normalExportProject
 fi
